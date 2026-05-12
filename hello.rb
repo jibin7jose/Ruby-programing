72003591b2b00fe3cee180f9606a9c27dbe65333
jibin jose <jibinjose884@gmail.com>
2026-05-12 22:32:41 +0530

    Create hello.rb to print greeting messages
    Add a Ruby program that prints greeting messages.

---
+++ hello.rb
@@ -0,0 +1,16 @@
+# -----------------------------------------
+# File Name: hello.rb
+# Description: Ruby program to print messages
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# ruby hello.rb
+
+# Output:
+# Hello, Ruby!
+# Welcome to Ruby programming.
+# -----------------------------------------
+
+puts "Hello, Ruby!"
+puts "Welcome to Ruby programming."
