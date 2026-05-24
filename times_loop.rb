c774ef193882f9d30ba99201c16d3095356d128f
jibin jose <jibinjose884@gmail.com>
2026-05-24 19:17:45 +0530

    Create times_loop.rb

---
+++ times_loop.rb
@@ -0,0 +1,20 @@
+# -----------------------------------------
+# File Name: times_loop.rb
+# Description: Ruby program demonstrating times loop
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# ruby times_loop.rb
+
+# Output:
+# Ruby is fun!
+# Ruby is fun!
+# Ruby is fun!
+# Ruby is fun!
+# Ruby is fun!
+# -----------------------------------------
+
+5.times do
+  puts "Ruby is fun!"
+end
