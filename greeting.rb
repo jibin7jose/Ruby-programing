c421afa9dc0be2e68b7647ae500b3dc09801c510
jibin jose <jibinjose884@gmail.com>
2026-08-26 19:10:06 +0530

    Create greeting.rb with greet method
    Add a Ruby program that demonstrates a greeting method.

---
+++ greeting.rb
@@ -0,0 +1,20 @@
+# -----------------------------------------
+# File Name: greeting.rb
+# Description: Ruby program demonstrating methods
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# ruby greeting.rb
+
+# Output:
+# Hello, welcome to Ruby!
+# Hello, welcome to Ruby!
+# -----------------------------------------
+
+def greet
+  puts "Hello, welcome to Ruby!"
+end
+
+greet
+greet
