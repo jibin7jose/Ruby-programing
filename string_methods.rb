2b0396da8f73e72cd9c24bb35e66238cc58c4792
jibin jose <jibinjose884@gmail.com>
2026-08-27 18:43:09 +0530

    Add string_methods.rb to demonstrate string methods
    This Ruby program demonstrates various string methods such as length, uppercase, lowercase, and reverse.

---
+++ string_methods.rb
@@ -0,0 +1,24 @@
+# -----------------------------------------
+# File Name: string_methods.rb
+# Description: Ruby program demonstrating string methods
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# ruby string_methods.rb
+
+# Output:
+# Original: Ruby Programming
+# Length: 16
+# Uppercase: RUBY PROGRAMMING
+# Lowercase: ruby programming
+# Reverse: gnimmargorP ybuR
+# -----------------------------------------
+
+text = "Ruby Programming"
+
+puts "Original: #{text}"
+puts "Length: #{text.length}"
+puts "Uppercase: #{text.upcase}"
+puts "Lowercase: #{text.downcase}"
+puts "Reverse: #{text.reverse}"
