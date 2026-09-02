8b29892a2031446349aaca497968c364de89978d
jibin jose <jibinjose884@gmail.com>
2026-09-02 17:30:43 +0530

    Add range_numbers.rb to demonstrate ranges in Ruby
    This Ruby program demonstrates the use of ranges and the each loop to print numbers from 1 to 10.

---
+++ range_numbers.rb
@@ -0,0 +1,27 @@
+# -----------------------------------------
+# File Name: range_numbers.rb
+# Description: Ruby program demonstrating ranges and each loop
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# ruby range_numbers.rb
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# -----------------------------------------
+
+numbers = 1..10
+
+numbers.each do |number|
+  puts number
+end
