bdf38299b527c490a17c84ff377afa3b060d13bb
jibin jose <jibinjose884@gmail.com>
2026-09-10 18:01:20 +0530

    Add Ruby program demonstrating until loop
    This Ruby program demonstrates the use of an until loop to print numbers from 1 to 5.

---
+++ until_loop.rb
@@ -0,0 +1,26 @@
+# -----------------------------------------
+# File Name: until_loop.rb
+# Description: Ruby program demonstrating until loop
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# ruby until_loop.rb
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# -----------------------------------------
+
+number = 1
+
+until number > 5
+
+  puts number
+
+  number += 1
+
+end
