dc4a96d2bf259311233e1f016f76012b7ed951cb
jibin jose <jibinjose884@gmail.com>
2026-09-13 19:04:52 +0530

    Add Ruby program for nested loops and number pattern
    This Ruby program demonstrates nested for loops to print a number pattern from 1 to 5.

---
+++ number_pattern.rb
@@ -0,0 +1,26 @@
+# -----------------------------------------
+# File Name: number_pattern.rb
+# Description: Ruby program demonstrating nested for loops and number pattern
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# ruby number_pattern.rb
+
+# Output:
+# 1
+# 12
+# 123
+# 1234
+# 12345
+# -----------------------------------------
+
+for i in 1..5
+
+  for j in 1..i
+    print j
+  end
+
+  puts
+
+end
